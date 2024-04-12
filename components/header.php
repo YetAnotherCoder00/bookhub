3f76e8624d1863cf0dd4712249269a5267af2681
Rizzler <rizzler@localhost.localdomain>
2024-04-13 00:58:29 +0200

    dsadsa

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <style>
 #header_container { 
   background-color:#1C1D36;
-  max-height: 8.5vh;
+  height: 8.5vh;
 }
 
 .logo {
@@ -22,157 +22,21 @@ body {
   display: inline-block;
 }
 
-
-
-.btn {
-  padding-top: 6vh;
-  transform: translate(-50%, -50%);
-  width: 80px;
-  cursor: pointer;
-}
-
-span {
-  display: block;
-  width: 100%;
-  box-shadow: 0 2px 10px 0 rgba(0,0,0,0.3);
-  border-radius: 3px;
-  height: 8px;
-  background: #878787;
-  transition: all .3s;
-  position: relative;
-}
-
-span + span {
-  margin-top: 14px;
-}
-
-.active span:nth-child(1) {
-  animation: ease .7s top forwards;
-}
-
-.not-active span:nth-child(1) {
-  animation: ease .7s top-2 forwards;
-}
-
-.active span:nth-child(2) {
-  animation: ease .7s scaled forwards;
-}
-
-.not-active span:nth-child(2) {
-  animation: ease .7s scaled-2 forwards;
-}
-
-.active span:nth-child(3) {
-  animation: ease .7s bottom forwards;
-}
-
-.not-active span:nth-child(3) {
-  animation: ease .7s bottom-2 forwards;
-}
-
-@keyframes top {
-  0% {
-    top: 0;
-    transform: rotate(0);
-  }
-  50% {
-    top: 22px;
-    transform: rotate(0);
-  }
-  100% {
-    top: 22px;
-    transform: rotate(45deg);
-  }
-}
-
-@keyframes top-2 {
-  0% {
-    top: 22px;
-    transform: rotate(45deg);
-  }
-  50% {
-    top: 22px;
-    transform: rotate(0deg);
-  }
-  100% {
-    top: 0;
-    transform: rotate(0deg);
-  }
-}
-
-@keyframes bottom {
-  0% {
-    bottom: 0;
-    transform: rotate(0);
-  }
-  50% {
-    bottom: 22px;
-    transform: rotate(0);
-  }
-  100% {
-    bottom: 22px;
-    transform: rotate(135deg);
-  }
-}
-
-@keyframes bottom-2 {
-  0% {
-    bottom: 22px;
-    transform: rotate(135deg);
-  }
-  50% {
-    bottom: 22px;
-    transform: rotate(0);
-  }
-  100% {
-    bottom: 0;
-    transform: rotate(0);
-  }
-}
-
-@keyframes scaled {
-  50% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(0);
-  }
-}
-
-@keyframes scaled-2 {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-
 .placeholder {
-    width: 80px;
+  width:10%;
+}
+
+.menu {
+  width:10%;
 }
 
 
 </style>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
 
 
-var btn = $('.btn');
 
-(function($) {
-$(document).ready(function() {
-    $(".btn").click(function(){
-        $(this).toggleClass('active not-active');
-    });
-});
-})(jQuery);
 
-</script>
 
 <body>
  
@@ -180,18 +44,21 @@ $(document).ready(function() {
 
     <div class="placeholder">
       <p></p>
-    </div> 
+    </div>
+
 
     <div class="logo inline">
       <a href="/"><img src="../assets/logo.png"></a>
     </div>
 
-    <div class="btn not-active inline">
-      <span></span>
-      <span></span>
-      <span></span>
+    <div class="menu">
+      <a href="/">Home</a>
+      <a href="/search">Search</a>
     </div>
 
+
+ 
   </div>
+
 
 </body>
