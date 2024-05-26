2e98e1fca28e175b71c3dee584e123b317c4bb95
Leonardo <leonardo@kreienbuehl.net>
2024-05-26 22:38:00 +0200

    creating users works

--- admin/create.php
+++ admin/create.php
@@ -6,13 +6,38 @@ if (!isset($_SESSION["username"]) || $_SESSION["username"] != "admin") {
 	die();
 }
 
+$conn = connectToDb();
+
 echo "welcome <br>";
 
 if (isset($_POST["create"])) {
   if ($_POST["create"] == "user") {
     // handle user
 
-    if (!isset($_POST["username"], $_POST["name"], $_POST["vornaame"], $_POST["password"], $_POST["email"])) {
+    if (isset($_POST["name"], $_POST["vorname"], $_POST["password"], $_POST["email"], $_POST["geburtstag"],
+      $_POST["geschlecht"])) {
+      echo "set enough";
+			$name = htmlspecialchars(trim($_POST["name"]));
+			$vorname = htmlspecialchars(trim($_POST["vorname"]));
+			$email = htmlspecialchars(trim($_POST["email"]));
+			$geburtstag = htmlspecialchars(trim($_POST["geburtstag"]));
+      $kontaktperemail = isset($_POST["kontaktperemail"]);
+      $geschlect = $_POST["geschlecht"];
+      $currentDate = date("Y-m-d");
+
+      // kid doesn't have a default / automatic value, so here is a messy workaround
+      $result = $conn->query("SELECT * FROM kunden ORDER BY kid DESC LIMIT 1");
+      $kid = $result->fetch_row()[0] + 1;
+
+      // yes, everything *needs* to be filled out, nothing has a default / automatic value
+      $preparedQuery = $conn->prepare("INSERT INTO kunden (
+        kid, name, vorname, email, geburtstag, kontaktpermail, kunde_seit, geschlecht) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+      $preparedQuery->bind_param("issssiss", $kid, $name, $vorname, $email, $geburtstag,
+        $kontaktperemail, $currentDate, $geschlect);
+      $preparedQuery->execute();
+    }
+    else {
       echo "not set enough";
     }
   }
@@ -24,15 +49,20 @@ if (isset($_POST["create"])) {
 <br>
 You can create something here
 
-create user!
+create customer!
 <form action="create.php" method="post">
 <!--  <input type="checkbox" hidden="hidden" checked="checked" name="creating" value="user">-->
-	<input type="text" name="username" placeholder="username">
-  <input type="text" name="name" placeholder="name">
-  <input type="text" name="vorname" placeholder="vorname">
-  <input type="text" name="password" placeholder="passwort">
-  <input type="email" name="email" placeholder="email">
-  <input type="checkbox" name="admin" placeholder="admin">
+  <input type="text" name="name" placeholder="name" required="required">
+  <input type="text" name="vorname" placeholder="vorname" required="required">
+  <input type="password" name="password" placeholder="password" required="required">
+  <input type="email" name="email" placeholder="email" required="required">
+  <input type="date" name="geburtstag" placeholder="geburtstag" required="required">
+  M:
+  <input type="radio" name="geschlecht" value="M">
+  F:
+  <input type="radio" name="geschlecht" value="F">
+  kontakt per email:
+  <input type="checkbox" name="kontaktperemail" placeholder="kontaktperemail">
   <button type="submit" name="create" value="user">submit the flesh of the user</button>
 </form>
 
@@ -57,5 +87,9 @@ create book!
 </form>
 
 <?php
+
+if (isset($_POST["create"])){
+  echo $_POST["create"];
+}
 
 ?>
