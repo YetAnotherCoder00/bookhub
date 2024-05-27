91f957f954ffa17f6b52c58c2bac7691173d48f9
Gamer2012 <65019724+YetAnotherCoder00@users.noreply.github.com>
2024-05-27 22:48:47 +0200

    deletion works

---
+++ admin/delete.php
@@ -0,0 +1,23 @@
+<?php
+include "../server/mysqlInterface.php";
+
+$conn = connectToDb();
+
+if (!isset($_SESSION["admin"]) || $_SESSION["admin"] !== true || (isset($_GET["delete"]) && $_GET["delete"] === "0")) {
+    header("Location: ../index.php");
+    die();
+}
+
+if (isset($_GET["delete"]) && $_GET["delete"] === "1") {
+    $conn->query("DELETE FROM buecher WHERE id = " . $_GET["id"]);
+    header("Location: ../index.php");
+    die();
+}
+
+?>
+
+<h1>
+    Are you sure you want to delete this book?
+</h1>
+<a href="./delete.php?id=<?= $_GET["id"] ?>&delete=1">yes</a>
+<a href="./delete.php?id=<?= $_GET["id"] ?>&delete=0">no</a>
